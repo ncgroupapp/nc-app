d316384575fe534d27e4017bcc4206fc09904ba7
Nahuel Gil <ngil17github@gmail.com>
2026-02-09 19:45:08 -0300

    feat: Add `Navbar` component with desktop and mobile navigation, user dropdown, and logout functionality.

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -133,7 +133,7 @@ export function Navbar() {
                       <Link
                         href={item.href}
                         className={cn(
-                          "group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-8 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
+                          "group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-8 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 mb-1",
                           pathname === item.href &&
                             "bg-accent text-accent-foreground"
                         )}
